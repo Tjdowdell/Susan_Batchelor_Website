4b800b20aa1447b6bbd2745067508ba6d7f7b8ed
Tyler Dowdell <dowdellt@pimit.lan>
2016-08-23 15:34:52 +1000

    installed plugins and styled them

--- wp-content/themes/sydney-child/inc/child-header.php
+++ wp-content/themes/sydney-child/inc/child-header.php
@@ -2,18 +2,42 @@
 
 function child_sydney_custom_header(){ ?>
 
+<?php if (is_page( $page = 'About Susan' )) { ?>
+
+<div class="child-header-not-home container-fluid">
+<div class="row">
+<div
+	<div class="inner_left2 col-md-4">
+		<img src="http://localhost/wordpress/wp-content/uploads/2016/08/portrait.png" alt="Susan Batchelor Portrait" class='portrait'>
+	</div>
+
+	</div>
+</div>
+</div>
+
+<?php } else { ?>
+
 <div class="child-header-home container-fluid">
 <div class="row">
 	<div class="inner_left col-md-4">
 		<img src="http://localhost/wordpress/wp-content/uploads/2016/08/portrait.png" alt="Susan Batchelor Portrait" class='portrait'>
+
 	</div>
-	<div class="inner_right col-md-8" >
+	<div class='col-xs-2'></div>
+	<div class="inner_right col-md-6" >
 		<h1><?php bloginfo('name'); ?><span><img class="name-stars-header" src="http://localhost/wordpress/wp-content/uploads/2016/08/stars.png"></span></h1>
 		<h4><?php bloginfo('description'); ?></h4>
+
+		<div class='subscribe'>
+			<h4 class='call-action'>Subscribe To My Blog</h4>
+			<?php  echo do_shortcode('[mc4wp_form id="70"]') ?>
+		</div>
 	</div>
 </div>
 </div>
+
 <?php 
+}
 };
 ?>
 
